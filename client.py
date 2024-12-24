3f29349bbcf6dab341c555476c9669c276093b39
david <davidlima@ufba.br>
2024-12-24 14:52:10 -0300

    First commit

---
+++ client.py
@@ -0,0 +1,53 @@
+# TODO
+#  - conexao com o servidor para implementar as funcionalidades e requisitos do projeto
+
+# - cada cliente se comunica com o servidor, que gerenciara a comunicacao entre clientes
+# - cada cliente deve se cadastrar junto ao servidor como um usuario
+# - cada cliente deve poder se comunicar com outro cliente usando o nome de usuario (semelhante ao que ocorre no WhatsApp atraves do numero de telefone)
+# - (OPCIONAL) clientes podem se juntar a grupos multicast (semelhante ao que ocorre no whatsapp)
+
+import socket
+from typing import *
+
+MSGLEN = 2048
+
+class Cliente:
+    """
+    Classe cliente.
+    Baseado em: https://docs.python.org/3/howto/sockets.html
+    """
+
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connected = False
+
+    def connect(self, host, port):
+        if not self.connected:
+            self.socket.connect((host, port))
+            self.connected = True
+        return self.connected
+
+    def disconnect(self):
+        self.socket.shutdown(0)
+        self.socket.close()
+
+    def enviar(self, msg):
+        self.socket.sendall(msg)
+        data = self.socket.recv(1024)
+        print(f"[{self.socket.getsockname()}] Received: {data}")
+
+    def receber(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+
+if __name__ == "__main__":
+    c = Cliente()
+    c.connect(socket.gethostname(), 8080)
+    c.enviar(b"Tentando conectar!")
---
+++ server.py
@@ -0,0 +1,59 @@
+# TODO
+#  - criar um servidor para implementar as funcionalidades e requisitos do projeto
+
+import socket
+from typing import *
+
+ADDRESS = (socket.gethostname(), 8080)
+CONN_MSG = b"Success. Connected to server process."
+
+class Servidor():
+    """
+    Classe do servidor.
+    Baseado em: https://docs.python.org/3/howto/sockets.html
+    """
+    def __init__(self, address, max_connections = 5):
+        self.log_tag = "[SERVER]"
+        self.max_connections = max_connections
+
+        self.log("Setting up server...")
+        self.socket = self._init_socket(address)
+        self.log("Server is up. Waiting for connections...")
+
+    def _init_socket(self, address) -> socket.socket:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(address)
+        s.listen(self.max_connections);
+        return s
+
+    def log(self, msg, **args):
+        print(self.log_tag, msg, **args)
+
+    def forwardMessage(self, src, dst, msg):
+        self.log(f"{src} -> {dst} ({len(msg)} bytes)")
+        total_sent = 0
+        while total_sent < len(msg):
+            sent = self.socket.send(msg[total_sent:])
+            if sent == 0:
+                raise RuntimeError("Socket connection broken.")
+            total_sent += sent
+        print("Message")
+
+    def onClientConnect(self, client_socket, client_addr):
+        print(f" >>> Connection received from {client_addr}")
+        data = client_socket.recv(1024)
+        if not data:
+            return
+        self.log(f"Received: {data}")
+
+    def init(self):
+        while True:
+            (c_socket, c_address) = self.socket.accept()
+            self.onClientConnect(c_socket, c_address)
+
+def main():
+    serv = Servidor(ADDRESS)
+    serv.init()
+
+if __name__ == "__main__":
+    main()
